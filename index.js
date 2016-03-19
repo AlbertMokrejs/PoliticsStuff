6c47ba73c297562f282cb6161c365fa9e2c7bc6f
Albert Mokrejs <albertmokrejs@gmail.com>
2016-03-19 13:28:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -29,13 +29,13 @@ republicans = makePartyData(processDels(republicans),statenames);
 democrats = makePartyData(processDels(democrats),statenames);
 
 d3.selectAll("#demtable")
-    .select("div")
+    .selectAll("div")
     .data(democrats)
     .enter()
     .append("div")
     .attr("class","bar");
 d3.selectAll("#demtable")
-    .select("div")
+    .selectAll("div")
     .each( function(){
         d3.select(this)
         .select("span")
@@ -45,7 +45,7 @@ d3.selectAll("#demtable")
         .attr("class", function(d){return d;});
     });
 d3.selectAll("#demtable")
-    .select("div")
+    .selectAll("div")
     .each( function(d){
         d3.select(this).select("#left")
         .text(function(d){return d;});
